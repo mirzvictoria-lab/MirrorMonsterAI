c6acfff73dff347b7afd77b234ad4fdb7898ef5f
Victoria <mirzvictoria@gmail.com>
2025-12-03 21:30:43 -0500

    Fix Express wildcard route for production

--- server/index-prod.ts
+++ server/index-prod.ts
@@ -2,7 +2,7 @@ import fs from "node:fs";
 import { type Server } from "node:http";
 import path from "node:path";
 
-import express, { type Express, type Request } from "express";
+import express, { type Express } from "express";
 
 import runApp from "./app";
 
@@ -11,15 +11,15 @@ export async function serveStatic(app: Express, server: Server) {
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  // Correct wildcard handler for Express v5
+  app.get("/*", (_req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
 
